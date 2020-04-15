9d9183eca89c798dd1a6ba756088a0d08759f74e
Juan <jmvieirar@gmail.com>
2020-04-15 23:13:00 +1000

    xstate example for loading data

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,102 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
+import { useMachine } from '@xstate/react';
+import { Machine, assign } from 'xstate';
 
+// @ts-ignore
 import styles from './index.module.scss';
+
+const allData = new Array(25).fill(0).map((_val, i) => i + 1);
+const perPage = 10;
+
+// @ts-ignore
+const dataMachine = Machine({
+  id: 'dataMachine',
+  initial: 'loading',
+  context: {
+    data: [],
+  },
+  states: {
+    loading: {
+      invoke: {
+        id: 'dataLoader',
+        src: (context, _event) => {
+          return (callback, _onEvent) => {
+            setTimeout(() => {
+              const { data } = context;
+              const newData = allData.slice(data.length, data.length + perPage);
+              const hasMore = newData.length === perPage;
+              if (hasMore) {
+                callback({ type: 'DONE_MORE', newData });
+              } else {
+                callback({ type: 'DONE_COMPLETE', newData });
+              }
+              // on real call will catch error and callback to fail event
+            }, 1000);
+          };
+        },
+      },
+      on: {
+        DONE_MORE: {
+          target: 'more',
+          actions: assign({
+            // data: (context, event) => [...context.data, ...event.newData],
+            data: ({ data }, { newData = [] }) => [...data, ...newData],
+          }),
+        },
+        DONE_COMPLETE: {
+          target: 'complete',
+          actions: assign({
+            // data: (context, event) => [...context.data, ...event.newData],
+            data: ({ data }, { newData = [] }) => [...data, ...newData],
+          }),
+        },
+        FAIL: 'failure',
+      },
+    },
+    more: {
+      on: {
+        LOAD: 'loading',
+      },
+    },
+    complete: {
+      type: 'final',
+    },
+    failure: {
+      type: 'final',
+    },
+  },
+});
 
 const App = () => {
   // properties
-  const [counter, setCounter] = useState<number>(0);
+  const [current, send] = useMachine(dataMachine);
+  const { data } = current.context;
 
   // methods
-  const handleClick = () => {
-    setCounter((currCounter) => currCounter + 1);
-  };
 
   // render
   return (
     <main className={styles.main}>
-      <h2>This is react</h2>
-      <span>Counter: {counter}</span>
-      <button onClick={handleClick}>Increase</button>
+      <h2>XState</h2>
+      <h3>State: {current.value}</h3>
+      <ul>
+        {data.map((row) => (
+          <li key={row}>{row}</li>
+        ))}
+        {current.matches('loading') && <li>...loading</li>}
+        {current.matches('more') && (
+          <li>
+            <button
+              onClick={() => {
+                send('LOAD');
+              }}
+            >
+              Load More
+            </button>
+          </li>
+        )}
+      </ul>
     </main>
   );
 };
